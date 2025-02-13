3bb9561151cd082b4d541e3bab4344ab4f602c74
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2025-02-13 11:51:59 +0100

    [PN-13939] Feat: Creation of NotificationUnlocked table with its DAO

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -51,5 +51,15 @@ aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
     --provisioned-throughput \
         ReadCapacityUnits=10,WriteCapacityUnits=5
 
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name pn-NotificationUnlocked  \
+    --attribute-definitions \
+        AttributeName=pk,AttributeType=S \
+    --key-schema \
+        AttributeName=pk,KeyType=HASH \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
 
 echo "Initialization terminated"
