1e45108c6b14335f581f81a2dd5a4a6f4b3f8c70
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2025-02-14 11:50:11 +0100

    [PN-13937] fix

--- src/test/java/it/pagopa/pn/stream/middleware/dao/StreamStatsDaoIT.java
+++ src/test/java/it/pagopa/pn/stream/middleware/dao/StreamStatsDaoIT.java
@@ -2,12 +2,12 @@ package it.pagopa.pn.stream.middleware.dao;
 
 import it.pagopa.pn.stream.BaseTest;
 import it.pagopa.pn.stream.config.PnStreamConfigs;
+import it.pagopa.pn.stream.dto.stats.StatsTimeUnit;
 import it.pagopa.pn.stream.dto.stats.StreamStatsEnum;
 import it.pagopa.pn.stream.middleware.dao.dynamo.StreamStatsDaoImpl;
 import it.pagopa.pn.stream.middleware.dao.dynamo.entity.StreamStatsEntity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 
@@ -18,11 +18,17 @@ class StreamStatsDaoIT extends BaseTest.WithLocalStack {
     @Autowired
     StreamStatsDaoImpl webhookStatsDao;
 
-    @Mock
-    PnStreamConfigs.Stats stats;
-
     @Test
     void getItem() {
+        PnStreamConfigs.Stats pnStreamConfigsStats = new PnStreamConfigs.Stats();
+
+        pnStreamConfigsStats.setTtl(Duration.ofDays(30));
+        pnStreamConfigsStats.setTimeUnit(StatsTimeUnit.DAYS);
+        pnStreamConfigsStats.setSpanUnit(1);
+
+        PnStreamConfigs pnStreamConfigs = new PnStreamConfigs();
+        pnStreamConfigs.setStats(pnStreamConfigsStats);
+
         StreamStatsEntity entity = new StreamStatsEntity("paId", "streamId", StreamStatsEnum.NUMBER_OF_REQUESTS);
         webhookStatsDao.updateAtomicCounterStats(entity).block();
         StreamStatsEntity retrievedEntity = webhookStatsDao.getItem(Key.builder().partitionValue("paId#streamId#NUMBER_OF_REQUESTS").sortValue("sk1").build()).block();
@@ -34,7 +40,15 @@ class StreamStatsDaoIT extends BaseTest.WithLocalStack {
 
     @Test
     void updateAtomicCounterStats() {
-        stats.setTtl(Duration.ofDays(3));
+        PnStreamConfigs.Stats pnStreamConfigsStats = new PnStreamConfigs.Stats();
+
+        pnStreamConfigsStats.setTtl(Duration.ofDays(30));
+        pnStreamConfigsStats.setTimeUnit(StatsTimeUnit.DAYS);
+        pnStreamConfigsStats.setSpanUnit(1);
+
+        PnStreamConfigs pnStreamConfigs = new PnStreamConfigs();
+        pnStreamConfigs.setStats(pnStreamConfigsStats);
+
         StreamStatsEntity entity = new StreamStatsEntity("paId", "streamId", StreamStatsEnum.NUMBER_OF_REQUESTS);
         webhookStatsDao.updateAtomicCounterStats(entity).block();
         webhookStatsDao.updateAtomicCounterStats(entity).block();
@@ -47,7 +61,15 @@ class StreamStatsDaoIT extends BaseTest.WithLocalStack {
 
     @Test
     void updateCustomCounterStats() {
-        stats.setTtl(Duration.ofDays(3));
+        PnStreamConfigs.Stats pnStreamConfigsStats = new PnStreamConfigs.Stats();
+
+        pnStreamConfigsStats.setTtl(Duration.ofDays(30));
+        pnStreamConfigsStats.setTimeUnit(StatsTimeUnit.DAYS);
+        pnStreamConfigsStats.setSpanUnit(1);
+
+        PnStreamConfigs pnStreamConfigs = new PnStreamConfigs();
+        pnStreamConfigs.setStats(pnStreamConfigsStats);
+
         webhookStatsDao.updateCustomCounterStats("paId#streamId#NUMBER_OF_READINGS", "sk3", 5).block();
         StreamStatsEntity updatedEntity = webhookStatsDao.getItem(Key.builder().partitionValue("paId#streamId#NUMBER_OF_READINGS").sortValue("sk3").build()).block();
         assert updatedEntity != null;
