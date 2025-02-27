1b95a8bcfa8af24b7de316093e24f7825a557869
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2025-02-27 15:50:05 +0100

    fix tests and config

--- src/test/java/it/pagopa/pn/stream/service/impl/StreamsServiceImplTest.java
+++ src/test/java/it/pagopa/pn/stream/service/impl/StreamsServiceImplTest.java
@@ -13,6 +13,7 @@ import it.pagopa.pn.stream.middleware.dao.dynamo.entity.StreamEntity;
 import it.pagopa.pn.stream.middleware.dao.dynamo.mapper.DtoToEntityStreamMapper;
 import it.pagopa.pn.stream.middleware.externalclient.pnclient.externalregistry.PnExternalRegistryClient;
 import it.pagopa.pn.stream.service.SchedulerService;
+import it.pagopa.pn.stream.service.StreamStatsService;
 import it.pagopa.pn.stream.service.utils.StreamUtils;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
@@ -38,6 +39,7 @@ class StreamsServiceImplTest {
     private SchedulerService schedulerService;
     private StreamsServiceImpl webhookService;
     private PnExternalRegistryClient pnExternalRegistryClient;
+    private StreamStatsService streamStatsService;
 
     private final int maxStreams = 5;
 
@@ -49,6 +51,7 @@ class StreamsServiceImplTest {
         schedulerService = Mockito.mock(SchedulerService.class);
         Mockito.mock(StreamUtils.class);
         pnExternalRegistryClient = Mockito.mock(PnExternalRegistryClient.class);
+        streamStatsService = Mockito.mock(StreamStatsService.class);
 
         when(pnStreamConfigs.getScheduleInterval()).thenReturn(1000L);
         when(pnStreamConfigs.getMaxLength()).thenReturn(10);
@@ -61,7 +64,7 @@ class StreamsServiceImplTest {
         when(pnStreamConfigs.getDeltaCounter()).thenReturn(1000);
 
         webhookService = new StreamsServiceImpl(streamEntityDao, schedulerService, pnStreamConfigs
-            ,pnExternalRegistryClient);
+            ,pnExternalRegistryClient, streamStatsService);
 
         new DtoToEntityStreamMapper(pnStreamConfigs);
     }
