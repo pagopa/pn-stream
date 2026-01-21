be7205c532e9bc67bbbaf3396532804ddc3ffc07
ECARADM5R <matteo.caradonna@emeal.nttdata.com>
2026-01-21 17:09:53 +0100

    PN-17980 - clean unused code in application class

--- src/main/java/it/pagopa/pn/stream/PnStreamApplication.java
+++ src/main/java/it/pagopa/pn/stream/PnStreamApplication.java
@@ -2,9 +2,6 @@ package it.pagopa.pn.stream;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import static it.pagopa.pn.stream.utils.SpringApplicationUtils.buildSpringApplicationWithListener;
 
@@ -12,19 +9,7 @@ import static it.pagopa.pn.stream.utils.SpringApplicationUtils.buildSpringApplic
 @EnableScheduling
 public class PnStreamApplication {
 
-
     public static void main(String[] args) {
         buildSpringApplicationWithListener().run(args);
-    }
-
-
-    @RestController
-    @RequestMapping("/")
-    public static class RootController {
-
-        @GetMapping("/")
-        public String home() {
-            return "";
-        }
     }
 }
