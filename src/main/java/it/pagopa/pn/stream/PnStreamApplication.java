b08ac5c1654434f4230edbf90c48fe4c43d10627
ECARADM5R <matteo.caradonna@emeal.nttdata.com>
2026-01-21 16:45:28 +0100

    PN-17980 - Enabling Spring Analyzer

--- src/main/java/it/pagopa/pn/stream/PnStreamApplication.java
+++ src/main/java/it/pagopa/pn/stream/PnStreamApplication.java
@@ -1,17 +1,20 @@
 package it.pagopa.pn.stream;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static it.pagopa.pn.stream.utils.SpringApplicationUtils.buildSpringApplicationWithListener;
+
 @SpringBootApplication
+@EnableScheduling
 public class PnStreamApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(PnStreamApplication.class, args);
+        buildSpringApplicationWithListener().run(args);
     }
 
 
