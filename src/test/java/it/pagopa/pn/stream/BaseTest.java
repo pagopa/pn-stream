6af3331491c43a55cd74a8c8a2e94cb2f25fdade
empassaro <emanuele.passaro@emeal.nttdata.com>
2026-06-08 15:31:51 +0200

    [PN-16211] fix: config for junit with local stack

--- src/test/java/it/pagopa/pn/stream/BaseTest.java
+++ src/test/java/it/pagopa/pn/stream/BaseTest.java
@@ -1,10 +1,15 @@
 package it.pagopa.pn.stream;
 
+import it.pagopa.pn.api.dto.events.MomProducer;
+import it.pagopa.pn.stream.middleware.queue.producer.abstractions.streamspool.impl.SortEvent;
+import it.pagopa.pn.stream.middleware.queue.producer.abstractions.streamspool.impl.StreamEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 
 @Slf4j
@@ -19,7 +24,14 @@ public abstract class BaseTest {
     @ActiveProfiles("test")
     @Import(LocalStackTestConfig.class)
     public static class WithLocalStack {
+        @MockitoBean(name = "sortEventsActionProducer")
+        private MomProducer<SortEvent> sortEventsActionProducer;
 
+        @MockitoBean(name = "streamActionsEventProducer")
+        private MomProducer<StreamEvent> webhookActionsEventProducer;
+
+        @MockitoBean
+        private SqsAsyncClient sqsAsyncClient;
     }
 
 
