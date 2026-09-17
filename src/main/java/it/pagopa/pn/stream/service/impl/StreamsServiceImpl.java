e31b3374a0c22248e9d7ef093badd957135a7b72
DacunzoMa <matteo.dacunzo@emeal.nttdata.com>
2026-09-17 17:14:31 +0200

    PN-21530 feat: add validation for filter values in stream creation requests if WaitForAccepted

--- src/main/java/it/pagopa/pn/stream/service/impl/StreamsServiceImpl.java
+++ src/main/java/it/pagopa/pn/stream/service/impl/StreamsServiceImpl.java
@@ -33,7 +33,6 @@ import java.util.*;
 import java.util.function.Predicate;
 
 import static it.pagopa.pn.stream.middleware.dao.dynamo.entity.StreamRetryAfter.RETRY_PREFIX;
-import static it.pagopa.pn.stream.service.impl.StreamEventsServiceImpl.DEFAULT_CATEGORIES;
 import static it.pagopa.pn.stream.utils.CommunicationTypeUtils.getDefaultCommunicationType;
 
 @Service
@@ -79,9 +78,7 @@ public class StreamsServiceImpl extends PnStreamServiceImpl implements StreamsSe
                 })
                 .flatMap(streamCreationRequestV28 -> {
                     if (Boolean.TRUE.equals(streamCreationRequestV28.getWaitForAccepted())) {
-                        if (streamCreationRequestV28.getFilterValues() == null || streamCreationRequestV28.getFilterValues().isEmpty() ||
-                                streamCreationRequestV28.getFilterValues().stream().noneMatch(f -> f.equals(DEFAULT_CATEGORIES) || f.equals("REQUEST_ACCEPTED")))
-                            return Mono.error(new PnStreamForbiddenException("Not Allowed the creation of sorted streams without  DEFAULT or REQUEST_ACCEPTED filter"));
+                        return filterValuesValidator.validateFilterValuesIfWaitForAccepted(streamCreationRequestV28);
                     }
                     return Mono.just(streamCreationRequestV28);
                 })
